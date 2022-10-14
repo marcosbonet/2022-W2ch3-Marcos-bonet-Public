a3600ef2e968c6076be98793c227f939d7feffa7
marcos <marcos.bonet@live.com.ar>
2022-10-14 09:35:34 +0200

    newfuntions

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
-export function myLength(array) {
-    let count = 0;
-    for (let i = 0; array[i] !== undefined; i++) {
-        count += 1;
+//export function myLength(array) {
+//    //const zeroError = new Error('Error, it is not a array');
+//    let count = 0;
+//    // if (!Array.isArray(array)) {
+//    //   throw zeroError;
+//    //}
+//
+//    for (let i = 0; array[i] !== undefined; i++) {
+//        count += 1;
+//    }
+//    return count;
+//}
+//export function myPush(arrayTwo, element) {
+//    arrayTwo[myLength(arrayTwo)] = element;
+//
+//    return myLength(arrayTwo);
+//}
+//
+//export function myIndexOf(array, element) {
+//    for (let i = 0; i < array.length; i++) {
+//        if (array[i] === element) {
+//            return i;
+//        }
+//    }
+//
+//    return -1;
+//}
+//
+//export function myUnShift(array, element) {
+//    array[myLength(array)] = array[myLength(array)] + 1;
+//    array[0] = element;
+//    return myLength(array);
+//}
+export function myConcat(array1, array2) {
+    let array3 = [];
+    let j = 0;
+    for (let i = 0; i < array1.length + array2.length; i++) {
+        array3[i] = array1[i];
+        if (i >= array1.length) {
+            array3[i] = array2[j];
+            j++;
+        }
     }
-    return count;
-}
-export function myPush(arrayTwo, element, ){
-    arrayTwo[myLength(arrayTwo)] = element
-    
-    return myLength(arrayTwo)}
-
-export function myIndexOf(array,element){
-
-    for (let i = 0; i < array.length; i++) {
-       if(array[i]===element){
-        return i
-       }
-       return -1;
-    }
-
+    return array3;
 }
 
-
-
-
-
-// export function myUnShift(array,element){
-
-//     array[myLength(array)]=array[myLength(array)]+1;
-//     array[0]=element;
-//     return myLength(array)
-// }
-
-
-
-
-
+console.log(myConcat([1, 2, 3][(4, 5, 6)]));
 
 // export function myPop(arrayThree){
 //     const arrayThreecopy= [];
 //     const element='';
 
-//     for(let i= 0;i<arrayThree.length -1; i++){ 
+//     for(let i= 0;i<arrayThree.length -1; i++){
 
 // arrayThreecopy.push(elements)
-    
-    
+
 // }
 // console.log(arrayThreecopy)
 
-     
 // }
-
-
 
 // let sum = 0;
 // const numbers = [65, 44, 12, 4];
@@ -61,5 +66,3 @@ export function myIndexOf(array,element){
 // function myFunction(item) {
 //   sum += item;
 // }
-    
-
--- metodos.test.js
+++ metodos.test.js
@@ -1,4 +1,4 @@
-import { myLength, myPush, myIndexOf } from './index.js';
+import { myLength, myPush, myIndexOf, myUnShift } from './index.js';
 
 describe('Given mylength function', () => {
     test(`when parametres are  length, then should be 4`, () => {
@@ -7,7 +7,19 @@ describe('Given mylength function', () => {
         const expectedResult = 4;
         expect(result).toBe(expectedResult);
     });
+    test ('is array is not a array throw a error',() => {
+        const string = 'pepe';
+        try {
+            myLength(string);
+        }catch (e){
+            expect(e.message).toBe(
+                'Error, it is not a array'
+            )
+        }
+    }
+    )
 });
+
 
 
 describe('Given myPush function', () => {
@@ -22,22 +34,30 @@ describe('Given myPush function', () => {
     });
 })
 
-describe('Given myPush function', () => {
+describe('Given indexof function', () => {
     test(`when parametres are  element, then should be arrayTwo `, () => {
         let array = [1, 2, 3, 4, 5];
-        let element= 4;
+        let element= 2;
         const result = myIndexOf(array,element);
-        expect(result).toBe(3);
+        expect(result).toBe(1);
+    });
+
+    test(`when the element is not inside of the array `, () => {
+        const array = [1, 2, 3, 4];
+        const element = 'Pepe';
+        const expectedResult = -1;
+        const result = myIndexOf(array, element);
+        expect(result).toBe(expectedResult);
     });
 })
 
-// describe('Given unShift function', () => {
+describe('Given unShift function', () => {
 
-//     test(`when parametres are  element, then should be arrayTwo `, () => {
-//         let array = [1, 2, 3, 4];
-//         let element= 9;
-//         const result = unShift (array,element);
+test(`when parametres are  element, then should be arrayTwo `, () => {
+        let array = [1, 2, 3, 4];
+        let element= 9;
+        const result = myUnShift(array, element);
        
-//         expect(result).toBe(5);
-//     });
-// })
+        expect(result).toBe(5);
+    });
+})
